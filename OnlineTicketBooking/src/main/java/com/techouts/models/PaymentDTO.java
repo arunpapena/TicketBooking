0fe53f24bfa3625ed7fab739c82b7bd6cbf34494
TO-PW7-01 <TO-PW7-01@TO-PW7-01-PC>
2018-10-29 19:23:49 +0530

    Cleared All the Errors on Controller,Services and DTO Clases.

--- OnlineTicketBooking/src/main/java/com/techouts/models/PaymentDTO.java
+++ OnlineTicketBooking/src/main/java/com/techouts/models/PaymentDTO.java
@@ -1,9 +1,10 @@
 package com.techouts.models;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import org.hibernate.annotations.Entity;
+
 
 @Entity
 @Table(name = "payment")
